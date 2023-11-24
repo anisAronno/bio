4c7a993dbf27dd000b0f1a0a30dd19a8278400cb
MD. Anichur Rahaman <38912435+anisAronno@users.noreply.github.com>
2023-11-24 19:16:59 +0600

    Add Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class', 
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    "system-ui",
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    '"Segoe UI"',
+                    "Roboto",
+                    '"Helvetica Neue"',
+                    "Arial",
+                    '"Noto Sans"',
+                    "sans-serif",
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"',
+                ],
+            },
+        },
+    },
+    plugins: [],
+}
+
